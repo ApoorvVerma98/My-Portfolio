5e7a8b267d111c726a6bbbedfcfe9bbe9681ef9f
ApoorvVerma98 <apoorv.verma9@gmail.com>
2023-08-09 22:34:37 +0530

    Changes in Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Apoorv Verma Resume.pdf";
+import pdf from "../../Assets/../Assets/ApoorvVermaResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
